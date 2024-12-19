ebef939b3bf980ac9ccdabe6b579228beb98658a
kristina-jovanovic <jovanovick25@gmail.com>
2024-12-19 21:20:32 +0100

    check if the colors match added

--- src/my_closet/core.clj
+++ src/my_closet/core.clj
@@ -5,3 +5,12 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(def color-rules
+  {:white #{:universal}
+   :black #{:universal}})
+
+(defn colors-match? [color1 color2]
+  (or (= (get color-rules color1 #{}) #{:universal})
+      (= (get color-rules color2 #{}) #{:universal})
+      (contains? (get color-rules color1 #{}) color2)))
--- test/my_closet/core_test.clj
+++ test/my_closet/core_test.clj
@@ -1,7 +1,9 @@
 (ns my-closet.core-test
   (:require [clojure.test :refer :all]
-            [my-closet.core :refer :all]))
+            [my-closet.core :refer :all]
+            [midje.sweet :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(fact "Check if the colors match"
+      (colors-match? :white :black) => true
+      (colors-match? :blue :green) => false)
+
