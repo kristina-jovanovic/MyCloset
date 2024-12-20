84e1b04e3698a49e2209980bebd9efb64ef21ee2
kristina-jovanovic <jovanovick25@gmail.com>
2024-12-20 17:21:17 +0100

    check if the seasons match added

--- src/my_closet/core.clj
+++ src/my_closet/core.clj
@@ -6,11 +6,21 @@
   [& args]
   (println "Hello, World!"))
 
+;defining color rules - every color has a set of colors that matches
 (def color-rules
   {:white #{:universal}
-   :black #{:universal}})
+   :black #{:universal}
+   :blue  #{:white :black :beige :grey}
+   :beige #{:universal}})
 
+;checking if the colors match
 (defn colors-match? [color1 color2]
   (or (= (get color-rules color1 #{}) #{:universal})
       (= (get color-rules color2 #{}) #{:universal})
       (contains? (get color-rules color1 #{}) color2)))
+
+;checking if the seasons match
+(defn seasons-match? [piece1 piece2]
+  (or (= (:season piece1) :universal)
+      (= (:season piece2) :universal)
+      (= (:season piece1) (:season piece2))))
--- test/my_closet/core_test.clj
+++ test/my_closet/core_test.clj
@@ -7,3 +7,9 @@
       (colors-match? :white :black) => true
       (colors-match? :blue :green) => false)
 
+(fact "Check if the seasons match"
+      (seasons-match? {:season :summer} {:season :summer}) => true
+      (seasons-match? {:season :summer} {:season :winter}) => false
+      (seasons-match? {:season :universal} {:season :summer}) => true
+      (seasons-match? {:season :winter} {:season :universal}) => true)
+
